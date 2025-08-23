c90f1d59f27419bd0400023619e0f029fc188e75
k7le-777 <kyleburns7.kb@yahoo.com>
2025-08-23 18:07:20 +0100

    Milestone: Core ToDo and Project classes completed with notes, completion states, and update functionality

--- src/modules/project.js
+++ src/modules/project.js
@@ -6,9 +6,11 @@ export default class Project {
     this.todos = [...todos];
     this._id = id;
   }
+
   getTitle() {
     return this.title;
   }
+
   setTitle(newTitle) {
     if (typeof newTitle === "string" && newTitle.trim() !== "") {
       this.title = newTitle;
@@ -17,13 +19,35 @@ export default class Project {
     }
   }
 
-  //methods
-  addTodo(todo) {
-      this.todos.push(todo);
+  getId() {
+    return this._id;
   }
+
+  addTodo(todo) {
+    if (todo instanceof ToDo) {
+      this.todos.push(todo);
+    } else {
+      console.warn("Only ToDo instances can be added");
+    }
+  }
+
+  removeTodo(id) {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    } else {
+      console.warn(`No todo found with id: ${id}`);
+    }
+  }
+
+  getTodo(id) {
+    return this.todos.find((todo) => todo.id === id);
+  }
+
   getAllTodos() {
     return [...this.todos];
   }
+
   updateTodo(id, updatedData) {
     const todo = this.getTodo(id);
     if (todo) {
--- src/modules/testProject.js
+++ src/modules/testProject.js
@@ -1,25 +1,39 @@
 import Project from "./project.js";
 import ToDo from "./toDo.js";
-// Create a new Project
+
+// 1. Create a new project
 const workProject = new Project("Work Tasks");
 console.log("New project:", workProject);
 
-// Create todos
+// 2. Create new todos
 const todo1 = new ToDo("Finish report", "Due by Friday", "2025-08-20", "high");
 const todo2 = new ToDo("Email client", "", "2025-08-18", "normal");
 
-// Add todos to project
+// 3. Add todos to project
 workProject.addTodo(todo1);
 workProject.addTodo(todo2);
-
 console.log("Project after adding todos:", workProject.getAllTodos());
-workProject.addTodo({ title: "Not a ToDo" }); // should warn
-// Remove the first todo
-workProject.removeTodo(todo1.id);
-console.log("Project after removing todo1:", workProject.getAllTodos());
 
-// Try removing a non-existent ID
-workProject.removeTodo("fake-id"); // should warn
-// Update todo2
-workProject.updateTodo(todo2.id, { title: "Email VIP client", priority: "high" });
+// 4. Test notes
+todo1.addNote("Check data accuracy");
+todo1.addNote("Add charts");
+console.log("Todo1 notes after adding:", todo1.getNotes());
+
+todo1.removeNote(0);
+console.log("Todo1 notes after removing first note:", todo1.getNotes());
+
+// 5. Test completion state
+console.log("Todo2 completed?", todo2.completed);
+todo2.markComplete();
+console.log("Todo2 after markComplete:", todo2.completed);
+todo2.markIncomplete();
+console.log("Todo2 after markIncomplete:", todo2.completed);
+todo2.toggleCompleted();
+console.log("Todo2 after toggleCompleted:", todo2.completed);
+
+// 6. Update a todo
+workProject.updateTodo(todo2.id, {
+  priority: "high",
+  title: "Email VIP client",
+});
 console.log("Project after updating todo2:", workProject.getAllTodos());
